4fa36a8fd18d5f7913c2e94f6d6212b1cd369dcd
KaiCYu <kaichyu@gmail.com>
2017-04-01 14:37:20 -0700

    rebasing  Please enter the commit message for your changes. Lines starting

--- public/src/components/TripSummary.jsx
+++ public/src/components/TripSummary.jsx
@@ -3,16 +3,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TripEntry from './TripEntry.jsx';
+<<<<<<< HEAD
 import Until from '../lib/util.js'
+=======
+import Util from '../lib/util.js'
+import { BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
+>>>>>>> attempting to fix recent trip link to membersummary
 
 class TripSummary extends React.Component {
   constructor(props) {
     super(props);
   }
 
+<<<<<<< HEAD
   showRecentTrip() {
     console.log('hi');
     Util.retrieveTripInfo()
+=======
+  showRecentTripReceipt(event) {
+    console.log(event.target.innerHTML);
+    //need to pass in trip name and admin name
+    // Util.retrieveTripInfo({adminName: this.props.username, tripName: event.target.innerHTML})
+    // {adminName: this.props.username, tripName: this.props.tripName}
+>>>>>>> attempting to fix recent trip link to membersummary
   }
 
   //ADD CLICK HANDLER TO RETREIVE STORED STATE FROM DB
@@ -23,7 +36,16 @@ class TripSummary extends React.Component {
         <h1>Most Recent Trips</h1>
         <div className='trip-summary'>{this.props.data.recent.map((item,index) => {
           return (
+<<<<<<< HEAD
             <p key={index} onClick={this.showRecentTrip}>{item.name}</p>
+=======
+              <Link to="/summary"
+                key={index}
+                onClick={this.showRecentTripReceipt}
+                >{item.name}
+              </Link> <br></br>
+            </div>
+>>>>>>> attempting to fix recent trip link to membersummary
           )
         })}
         </div>
